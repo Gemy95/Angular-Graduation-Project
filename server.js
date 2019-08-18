d029686d714f860bada30d9df09a79e904f50dc4
Gemy95 <ali.gamal95880@gmail.com>
2019-08-18 23:39:15 +0200

    new update

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/project1'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/project1/index.html'));
-
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/project1/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
