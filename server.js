4f1f477a12e8436134e30e48c14e4ed1b59bc968
Gemy95 <ali.gamal95880@gmail.com>
2019-08-18 22:04:10 +0200

    new update

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/project1'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/project1/index.html'));
+  res.send("dataaaaaaaaaa");  
+//res.sendFile(path.join(__dirname+'/dist/project1/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
